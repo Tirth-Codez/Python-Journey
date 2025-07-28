dce08f3ee60cebe947bd98737ef4b2929c196164
TirthCodez <tirthpopat1722@gmail.com>
2025-07-28 13:45:38 +0530

    Day 1 complete

---
+++ Month_1_Python_Foundation/Week1_basic/data_types.py
@@ -0,0 +1,9 @@
+x = 5          # int
+y = 3.14       # float
+z = "Python"   # str
+is_coding = True  # bool
+
+print(f"x: {x} ({type(x)})")
+print(f"y: {y} ({type(y)})")
+print(f"z: {z} ({type(z)})")
+print(f"is_coding: {is_coding} ({type(is_coding)})")
